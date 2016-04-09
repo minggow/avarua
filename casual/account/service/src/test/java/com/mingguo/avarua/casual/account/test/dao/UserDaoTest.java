a825f4eebdae96db7705407ad1646d46472829f5
wumingguo <971096804@qq.com>
2016-04-10 00:21:25 +0800

    测试用例调整格式

--- casual/account/service/src/test/java/com/mingguo/avarua/casual/account/test/dao/UserDaoTest.java
+++ casual/account/service/src/test/java/com/mingguo/avarua/casual/account/test/dao/UserDaoTest.java
@@ -2,56 +2,72 @@ package com.mingguo.avarua.casual.account.test.dao;
 
 import com.mingguo.avarua.casual.account.model.User;
 import com.mingguo.avarua.casual.account.service.repository.dao.UserDao;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by mingguo.wu on 2015/9/16.
  */
 public class UserDaoTest extends BaseDaoTest{
 
-    private static final String LOGIN_NAME1 = "wumingguo";
+    private static final String LOGIN_NAME1 = "wumingguo1";
     private static final String LOGIN_NAME2 = "wumingguo2";
+    private User user1;
+    private User user2;
 
     @Autowired
     private UserDao userDao;
 
-    @Before
+    @BeforeClass
     public void setUp() {
+        tearDown();
+        //init user1 & user2
+        user1.setLoginName(LOGIN_NAME1);
+        user1.setDescription("desc " + LOGIN_NAME1);
+        user1.setDisplayName(LOGIN_NAME1);
+        user1.setLoginCount(1);
+        user1.setPassword(LOGIN_NAME1 + " password");
+        user1.setLastLoginTime(new Date());
 
+        user2.setLoginName(LOGIN_NAME2);
+        user2.setDescription("desc " + LOGIN_NAME2);
+        user2.setLoginCount(2);
+        user2.setPassword(LOGIN_NAME2 + " password");
+        user2.setLastLoginTime(new Date());
     }
 
-    @After
+    @AfterClass
     public void tearDown() {
+        //delete ${LOGIN_NAME1}
+        User user1 = userDao.getUserByLoginName(LOGIN_NAME1);
+        if(user1 != null) { //start delete
+            int id = user1.getId();
+            Assert.assertEquals(1, userDao.deleteUsersByIds(Arrays.asList(id)));
+        }
+        //delete ${LOGIN_NAME2}
+        User user2 = userDao.getUserByLoginName(LOGIN_NAME2);
+        if(user2 != null) { //start delete
+            int id = user2.getId();
+            Assert.assertEquals(1, userDao.deleteUsersByIds(Arrays.asList(id)));
+        }
 
     }
 
     @Test
     public void testAddUser() {
-        User user = new User();
-        //set value
-        user.setLoginName("wumingguo2");
-        user.setDescription("user wumingguo");
-        user.setDisplayName("wumingguo");
-        user.setLoginCount(0);
-        user.setPassword("password");
-        user.setLastLoginTime(new Date());
-
-        System.out.println("addUser result:--->>> " + userDao.addUser(user));
+        //add user1
+        Assert.assertEquals(true, userDao.addUser(user1) > 0);
+        //add user2
+        Assert.assertEquals(true, userDao.addUser(user2) > 0);
     }
 
     @Test
     public void testGetUsers() {
         //根据loginName查询用户
-        User user1 = userDao.getUserByLoginName("wumingguo");
-        System.out.println("getUserByLoginName('wumingguo')------->>>>>>>\n" + GSON.toJson(user1));
+        User user1 = userDao.getUserByLoginName(LOGIN_NAME1);
+        Assert.assertNotEquals(null, user1);
 
         //根据id查询用户
         User user2 = userDao.getUserById(1);
